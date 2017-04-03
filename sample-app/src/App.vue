c3d15c11b9b4c007d8bc4099e41beb2b4006ce9c
Inndy <inndy.tw@gmail.com>
2017-04-03 15:48:53 +0800

    Add sample-app

---
+++ sample-app/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <h1>Clipboard.js binding for Vue2</h1>
+    <div>
+      <input type="text" v-model="message">
+
+      <button v-clipboard:copy="message"
+              v-clipboard:success="onCopied">Copy Text</button>
+
+      <a v-clipboard:copy="message"
+         v-clipboard:success="onCopied"
+         href="#">Copy Text</a>
+    </div>
+    <div>
+      <p>{{status}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      message: 'The text to copy',
+      status: 'Ready...'
+    }
+  },
+  methods: {
+    onCopied (e) {
+      this.status = `You just copied text: "${e.text}"`
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+a {
+  color: blue;
+}
+</style>
---
+++ sample-app/src/main.js
@@ -0,0 +1,16 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import VueClipboard from 'vue-clipboard2'
+
+Vue.use(VueClipboard)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})
