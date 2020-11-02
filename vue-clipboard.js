3788c60d12c5f22e50c0ec89e311fee50333ec16
JackWang <wyb12358@gmail.com>
2020-11-02 16:48:35 +0800

    Simple adaptation to vue3, can be used normally under vue3

--- vue-clipboard.js
+++ vue-clipboard.js
@@ -1,79 +1,93 @@
-var Clipboard = require('clipboard/dist/clipboard.min.js') // FIXME: workaround for browserify
+const Clipboard = require('clipboard/dist/clipboard.min.js') // FIXME: workaround for browserify
 
-var VueClipboardConfig = {
+const VueClipboardConfig = {
   autoSetContainer: false,
   appendToBody: true // This fixes IE, see #50
 }
+const $copyText = function (text, container) {
+  return new Promise(function (resolve, reject) {
+    const fakeElement = document.createElement('button')
+    const clipboard = new Clipboard(fakeElement, {
+      text: function () { return text },
+      action: function () { return 'copy' },
+      container: typeof container === 'object' ? container : document.body
+    })
+    clipboard.on('success', function (e) {
+      clipboard.destroy()
+      resolve(e)
+    })
+    clipboard.on('error', function (e) {
+      clipboard.destroy()
+      reject(e)
+    })
+    if (VueClipboardConfig.appendToBody) document.body.appendChild(fakeElement)
+    fakeElement.click()
+    if (VueClipboardConfig.appendToBody) document.body.removeChild(fakeElement)
+  })
+}
+const dBind = function (el, binding, vnode) {
+  if (binding.arg === 'success') {
+    el._vClipboard_success = binding.value
+  } else if (binding.arg === 'error') {
+    el._vClipboard_error = binding.value
+  } else {
+    const clipboard = new Clipboard(el, {
+      text: function () { return binding.value },
+      action: function () { return binding.arg === 'cut' ? 'cut' : 'copy' },
+      container: VueClipboardConfig.autoSetContainer ? el : undefined
+    })
+    clipboard.on('success', function (e) {
+      const callback = el._vClipboard_success
+      callback && callback(e)
+    })
+    clipboard.on('error', function (e) {
+      const callback = el._vClipboard_error
+      callback && callback(e)
+    })
+    el._vClipboard = clipboard
+  }
+}
+const dUpdate = function (el, binding) {
+  if (binding.arg === 'success') {
+    el._vClipboard_success = binding.value
+  } else if (binding.arg === 'error') {
+    el._vClipboard_error = binding.value
+  } else {
+    el._vClipboard.text = function () { return binding.value }
+    el._vClipboard.action = function () { return binding.arg === 'cut' ? 'cut' : 'copy' }
+  }
+}
+const dUnbind = function (el, binding) {
+  if (binding.arg === 'success') {
+    delete el._vClipboard_success
+  } else if (binding.arg === 'error') {
+    delete el._vClipboard_error
+  } else {
+    el._vClipboard.destroy()
+    delete el._vClipboard
+  }
+}
 
-var VueClipboard = {
+const VueClipboard = {
   install: function (Vue) {
-    Vue.prototype.$clipboardConfig = VueClipboardConfig
-    Vue.prototype.$copyText = function (text, container) {
-      return new Promise(function (resolve, reject) {
-        var fakeElement = document.createElement('button')
-        var clipboard = new Clipboard(fakeElement, {
-          text: function () { return text },
-          action: function () { return 'copy' },
-          container: typeof container === 'object' ? container : document.body
-        })
-        clipboard.on('success', function (e) {
-          clipboard.destroy()
-          resolve(e)
-        })
-        clipboard.on('error', function (e) {
-          clipboard.destroy()
-          reject(e)
-        })
-        if (VueClipboardConfig.appendToBody) document.body.appendChild(fakeElement)
-        fakeElement.click()
-        if (VueClipboardConfig.appendToBody) document.body.removeChild(fakeElement)
+    if (Vue.prototype) {
+      Vue.prototype.$clipboardConfig = VueClipboardConfig
+      Vue.prototype.$copyText = $copyText;
+
+      Vue.directive('clipboard', {
+        bind: dBind,
+        update: dUpdate,
+        unbind: dUnbind,
+      })
+    } else {
+      Vue.config.globalProperties.$clipboardConfig = VueClipboardConfig
+      Vue.config.globalProperties.$copyText = $copyText
+      Vue.directive('clipboard', {
+        beforeMount: dBind,
+        updated: dUpdate,
+        unmounted: dUnbind,
       })
     }
-
-    Vue.directive('clipboard', {
-      bind: function (el, binding, vnode) {
-        if (binding.arg === 'success') {
-          el._vClipboard_success = binding.value
-        } else if (binding.arg === 'error') {
-          el._vClipboard_error = binding.value
-        } else {
-          var clipboard = new Clipboard(el, {
-            text: function () { return binding.value },
-            action: function () { return binding.arg === 'cut' ? 'cut' : 'copy' },
-            container: VueClipboardConfig.autoSetContainer ? el : undefined
-          })
-          clipboard.on('success', function (e) {
-            var callback = el._vClipboard_success
-            callback && callback(e)
-          })
-          clipboard.on('error', function (e) {
-            var callback = el._vClipboard_error
-            callback && callback(e)
-          })
-          el._vClipboard = clipboard
-        }
-      },
-      update: function (el, binding) {
-        if (binding.arg === 'success') {
-          el._vClipboard_success = binding.value
-        } else if (binding.arg === 'error') {
-          el._vClipboard_error = binding.value
-        } else {
-          el._vClipboard.text = function () { return binding.value }
-          el._vClipboard.action = function () { return binding.arg === 'cut' ? 'cut' : 'copy' }
-        }
-      },
-      unbind: function (el, binding) {
-        if (binding.arg === 'success') {
-          delete el._vClipboard_success
-        } else if (binding.arg === 'error') {
-          delete el._vClipboard_error
-        } else {
-          el._vClipboard.destroy()
-          delete el._vClipboard
-        }
-      }
-    })
   },
   config: VueClipboardConfig
 }
