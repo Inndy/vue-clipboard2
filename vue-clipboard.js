f1c54d487ec35e1ec87c6ec9b21fb231b160127a
Inndy <inndy.tw@gmail.com>
2021-09-11 00:25:20 +0800

    Add FIXME comment

--- vue-clipboard.js
+++ vue-clipboard.js
@@ -64,6 +64,7 @@ var VueClipboard = {
         }
       },
       unbind: function (el, binding) {
+        // FIXME: investigate why $element._vClipboard was missing
         if(!el._vClipboard) return
         if (binding.arg === 'success') {
           delete el._vClipboard_success
